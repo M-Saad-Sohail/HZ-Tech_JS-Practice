1f6649c660f7b16ea4a8a116ea6da3977f57441b
m-saad-sohail <saadsohail232005@gmail.com>
2024-07-02 14:44:41 +0500

    for in loop

--- main.js
+++ main.js
@@ -5,7 +5,24 @@
 //     return squarer(num1) * squarer(num2)
 // }
 // console.log(squareMultiply(5, 6));
-for (let i = 0; i < 5; i++) {
-   
-    
-}
+// for (let i = 1; i < 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("Fizz Buzz");
+//     } else if (i % 3 == 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 == 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+// const users = {
+//     1:"saad",
+//     2:"saad2",
+//     3:"saad3",
+//     4:"saad4",
+//     5:"saad5",
+// }
+// for(let u in users){
+//     console.log(users[u]); 
+// }
