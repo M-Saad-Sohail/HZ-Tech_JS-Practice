079bc4d65b9fe7ecebbaac4fa62dcbaa049b7a21
m-saad-sohail <saadsohail232005@gmail.com>
2024-07-02 12:58:44 +0500

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function squareMultiply(num1, num2) {
-    function squarer(num) {
-        return num * num;
-    }
-    return squarer(num1) * squarer(num2)
-}
-console.log(squareMultiply(5, 6));
+// function squareMultiply(num1, num2) {
+//     function squarer(num) {
+//         return num * num;
+//     }
+//     return squarer(num1) * squarer(num2)
+// }
+// console.log(squareMultiply(5, 6));
