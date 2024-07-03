1e77359fc6762c0c555ef5635ec39f241661f2d0
m-saad-sohail <saadsohail232005@gmail.com>
2024-07-03 13:31:32 +0500

    rest patteren

--- main.js
+++ main.js
@@ -62,3 +62,9 @@
 // }
 // const [resultArray] = filteredArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18], 2); 
 // console.log(resultArray);
+
+// // REST PATTERN
+// let restPattern = (arguments, ...restOfArguments) => {
+//     return `${arguments} \n\n ${restOfArguments} `
+// }
+// console.log(restPattern("Saad Sohail","HZTECH",19,false,{12:"saad",13:"sohail"}));
